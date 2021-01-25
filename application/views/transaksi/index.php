ef328d70f21562650d9d5594198374a50ced7b6d
aliwafa16 <aliwafamuhamad16@gmail.com>
2021-01-25 08:39:10 +0700

    view transaksi 11%

--- application/views/transaksi/index.php
+++ application/views/transaksi/index.php
@@ -52,7 +52,7 @@
                             </div>
                         </div>
                         <div class="col-md-3">
-                            <button class="btn btn-primary" type="submit">Add</button>
+                            <button class="btn btn-primary" type="submit"><i class="fas fa-shopping-cart"></i> Add</button>
                         </div>
                     </div>
                 </div>
@@ -63,6 +63,53 @@
                 <div class="card-body" style="width: auto" style="height: 100px;">
                     <h5 class="card-title"> <strong>Nomor Invoice</strong></h5>
                     <h5>TRNSK25012021001</h5>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <div class="row">
+        <div class="col">
+            <table class="table table-hover mt-3">
+                <thead>
+                    <tr>
+                        <th scope="col">No</th>
+                        <th scope="col">Tanggal</th>
+                        <th scope="col">Kode barang</th>
+                        <th scope="col">Nama barang</th>
+                        <th scope="col">Quantity</th>
+                        <th scope="col">Harga satuan</th>
+                        <th scope="col">Diskon</th>
+                        <th scope="col">Sub total</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr>
+                        <th scope="row">1</th>
+                        <td>Mark</td>
+                        <td>Otto</td>
+                        <td>@mdo</td>
+                    </tr>
+                    <tr>
+                        <th scope="row">1</th>
+                        <td>Mark</td>
+                        <td>Otto</td>
+                        <td>@mdo</td>
+                    </tr>
+                </tbody>
+            </table>
+        </div>
+    </div>
+
+    <div class="row">
+        <div class="col-4">
+            <div class="card">
+                <h6 class="card-header">
+                    <strong>Total dan diskon</strong>
+                </h6>
+                <div class="card-body">
+                    <div class="row">
+                    </div>
                 </div>
             </div>
         </div>
